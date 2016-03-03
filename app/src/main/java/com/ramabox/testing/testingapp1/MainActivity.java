177b060b4f3ca7cb3576b38c7a21bece491a15bf
Lynn1995 <lynneke.dehaes@hotmail.com>
2016-03-03 15:26:34 +0100

    extra activity and uart communication

--- app/src/main/java/com/ramabox/testing/testingapp1/MainActivity.java
+++ app/src/main/java/com/ramabox/testing/testingapp1/MainActivity.java
@@ -1,15 +1,18 @@
 package com.ramabox.testing.testingapp1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        ETdata = (EditText)findViewById(R.id.editText);
     }
 
     @Override
@@ -58,8 +60,11 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void sendCheck(View view){
-        String data = ETdata.getText().toString();
-        Toast.makeText(getApplicationContext(), "The data will be send if it is implemented", Toast.LENGTH_LONG).show();
+
+        Toast.makeText(getApplicationContext(), "You will can send your data in a minute", Toast.LENGTH_LONG).show();
+
+
+        this.startActivity(new Intent(this, SendData.class));
 
     }
 }
